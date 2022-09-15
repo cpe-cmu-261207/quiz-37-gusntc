b62221e6af518d446ffe1cd2d314449db3d0154a
gusntc <Natacha_rungbanpant@cmu.ac.th>
2022-09-15 14:30:36 +0700

    finish

--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Reply(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className="d-flex gap-2 my-2 ps-5">
+        <img
+          src={props.userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{props.username}</span>
+          <br />
+          <span>{props.replyText}</span>
+          {props.likeNum > 0 ? (
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{props.likeNum}</span>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
